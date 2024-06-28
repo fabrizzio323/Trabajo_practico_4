3fd7ad03337baa3204584bf9cf1780c1e859cb57
USER <USER@DESKTOP-M3HP0J3>
2024-06-28 19:56:24 -0300

    AlumnoDTO junto a la clase mapper del alumno realizado

---
+++ Tp4_FabrizioArmada/src/main/java/ar/edu/unju/fi/mapper/AlumnoMapper.java
@@ -0,0 +1,33 @@
+package ar.edu.unju.fi.mapper;
+
+import java.util.List;
+
+import org.mapstruct.InheritConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+import ar.edu.unju.fi.dto.AlumnoDTO;
+import ar.edu.unju.fi.model.Alumno;
+
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface AlumnoMapper {
+    
+	@Mapping(source="dni", target="dni")
+	@Mapping(source="nombre", target="nombre")
+	@Mapping(source="apellido", target="apellido")
+	@Mapping(source="email", target="email")
+	@Mapping(source="telefono", target="telefono")
+	@Mapping(source="fechaNacimiento", target="fechaNacimiento", dateFormat="yyyy-MM-dd")
+	@Mapping(source="domicilio", target="domicilio")
+	@Mapping(source="lu", target="lu")
+	AlumnoDTO ToAlumnoDto(Alumno alumno);
+	
+	@InheritConfiguration
+	Alumno ToAlumno(AlumnoDTO alumnoDTO);
+     
+    List<AlumnoDTO> ToAlumnoDTOList(List<Alumno> alumnos);
+    
+    List<Alumno> ToAlumnoList(List<AlumnoDTO> alumnosDTO);
+}
