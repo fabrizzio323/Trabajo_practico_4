71e1553a3bde7c29ca79974e167de5f2ab676bc4
USER <USER@DESKTOP-M3HP0J3>
2024-07-08 18:07:14 -0300

    Validaciones agregadas a todos los objetos

--- Tp4_FabrizioArmada/src/main/java/ar/edu/unju/fi/dto/CarreraDTO.java
+++ Tp4_FabrizioArmada/src/main/java/ar/edu/unju/fi/dto/CarreraDTO.java
@@ -5,6 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import ar.edu.unju.fi.model.Materia;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +21,14 @@ import lombok.NoArgsConstructor;
 public class CarreraDTO {
 
 	private Long id;
+	@NotNull(message="El codigo no puede estar vacio")
 	private int codigo;
+	@NotBlank(message="El nombre no puede estar vacio")
+	@Size(min=5,max=20,message="el nombre debe tener entre 5 y 20 caracteres")
 	private String nombre;
+	@NotNull(message="La duracion no puede estar vacia")
+	@Min(value=3, message="La duracion minima es de 3 años")
+    @Max(value=5, message="La duracion maxima es de 6 años")
 	private short cantidadAnios;
 	private String estado;
 	//ver de cambiar boolean por String
--- Tp4_FabrizioArmada/src/main/java/ar/edu/unju/fi/dto/MateriaDTO.java
+++ Tp4_FabrizioArmada/src/main/java/ar/edu/unju/fi/dto/MateriaDTO.java
@@ -5,6 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import ar.edu.unju.fi.model.Alumno;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,12 +21,23 @@ import lombok.NoArgsConstructor;
 public class MateriaDTO {
 
 	private Long id;
+	@NotNull(message="El codigo no puede estar vacio")
 	private int codigo;
+	@NotBlank(message = "El nombre no puede ser nulo")
+    @Size(min = 1, max = 50, message = "El nombre debe tener entre 1 y 50 caracteres")
 	private String nombre;
+	@NotBlank(message = "El curso no puede ser nulo")
+    @Size(min = 1, max = 50, message = "El nombre debe tener entre 1 y 50 caracteres")
 	private String curso;
+	@NotNull(message = "La cantidad de horas no puede ser nula")
+    @Min(value = 0, message = "La cantidad mínima de horas es 0")
+    @Max(value = 24, message = "La cantidad máxima de horas es 24")
 	private short cantidadHoras;
+	@NotBlank(message="La modalidad no puede ser nula")
 	private String modalidad;
+	@NotNull(message="El campo docente no puede estar nulos")
 	private DocenteDTO docente;
+	@NotNull(message="El campo carrera no puede estar nulos")
 	private CarreraDTO carrera;
 	 private String estado;
 	 private List<Alumno> alumnos;
