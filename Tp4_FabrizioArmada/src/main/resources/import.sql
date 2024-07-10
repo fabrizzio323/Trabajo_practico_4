48b8d70bcadeddab178ac3a9bc99d118e9ba2624
USER <USER@DESKTOP-M3HP0J3>
2024-07-10 16:24:24 -0300

    Se agregaron mas objetos en el archivo import.sql

--- Tp4_FabrizioArmada/src/main/resources/import.sql
+++ Tp4_FabrizioArmada/src/main/resources/import.sql
@@ -1,12 +1,43 @@
 insert into carreras (C_codigo, C_nombre, C_cantidad_Anios, C_estado) values ('1001', 'APU', 3, b'1');
-insert into carreras (C_codigo, C_nombre, C_cantidad_Anios, C_estado) values ('1002', 'ING_INF', 5, b'1');
-insert into carreras (C_codigo, C_nombre, C_cantidad_Anios, C_estado) values ('1003', 'ING_QUI', 5, b'1');
+insert into carreras (C_codigo, C_nombre, C_cantidad_Anios, C_estado) values ('1002', 'ING_INFORMATICA', 5, b'1');
+insert into carreras (C_codigo, C_nombre, C_cantidad_Anios, C_estado) values ('1003', 'ING_QUIMICA', 5, b'1');
+insert into carreras (C_codigo, C_nombre, C_cantidad_Anios, C_estado) values ('1004', 'ING_BIOMEDICA', 5, b'1');
+insert into carreras (C_codigo, C_nombre, C_cantidad_Anios, C_estado) values ('1005', 'ING_MINA', 5, b'1');
+insert into carreras (C_codigo, C_nombre, C_cantidad_Anios, C_estado) values ('1006', 'TEC_VJUEGOS', 3, b'1');
+insert into carreras (C_codigo, C_nombre, C_cantidad_Anios, C_estado) values ('1007', 'ING_CIVIL', 5, b'1');
+insert into carreras (C_codigo, C_nombre, C_cantidad_Anios, C_estado) values ('1008', 'ING_AEROESPACIAL', 5, b'1');
+insert into carreras (C_codigo, C_nombre, C_cantidad_Anios, C_estado) values ('1009', 'ING_SOFTWARE', 5, b'1');
+insert into carreras (C_codigo, C_nombre, C_cantidad_Anios, C_estado) values ('1010', 'ING_ELECTRONICA', 5, b'1');
+
 insert into alumnos (A_dni, A_nombre, A_apellido, A_email, A_telefono, A_fecha_nacimiento, A_domicilio, A_lu, A_estado) values ('44621357','Tomas','Palacion','TomasPala@gmail.com','3886532987','02-04-06','calle propina','4917',b'1');
 insert into alumnos (A_dni, A_nombre, A_apellido, A_email, A_telefono, A_fecha_nacimiento, A_domicilio, A_lu, A_estado) values ('44256301','Jose','Guzman','Gusjose@gmail.com','3882564872','01-02-02','Calle pepin','4458',b'1');
 insert into alumnos (A_dni, A_nombre, A_apellido, A_email, A_telefono, A_fecha_nacimiento, A_domicilio, A_lu, A_estado) values ('44203369','pepa','pig','pepig@gmail.com','3880235669','99-06-02','calle matambre','4918',b'1');
+insert into alumnos (A_dni, A_nombre, A_apellido, A_email, A_telefono, A_fecha_nacimiento, A_domicilio, A_lu, A_estado) values ('43568954','Jeremias','sanchez','Jsanchez@gmail.com','3880236458','03-08-03','calle America','4867',b'1');
+insert into alumnos (A_dni, A_nombre, A_apellido, A_email, A_telefono, A_fecha_nacimiento, A_domicilio, A_lu, A_estado) values ('44896320','Lucas','martinez','Lmartinez@gmail.com','3886958471','98-10-02','Calle San Martin','4869',b'1');
+insert into alumnos (A_dni, A_nombre, A_apellido, A_email, A_telefono, A_fecha_nacimiento, A_domicilio, A_lu, A_estado) values ('44213569','Santiago','guzman','GSantiago@gmail.com','3880345879','99-06-02','calle ','4918',b'1');
+insert into alumnos (A_dni, A_nombre, A_apellido, A_email, A_telefono, A_fecha_nacimiento, A_domicilio, A_lu, A_estado) values ('44213654','Miguel','romeo','Romiguel@gmail.com','3880235668','03-03-02','calle Sarmiento','4756',b'1');
+insert into alumnos (A_dni, A_nombre, A_apellido, A_email, A_telefono, A_fecha_nacimiento, A_domicilio, A_lu, A_estado) values ('43568987','Fabricio','perez','Gusjose@gmail.com','3882365489','03-09-02','Calle Jujuy','4835',b'1');
+insert into alumnos (A_dni, A_nombre, A_apellido, A_email, A_telefono, A_fecha_nacimiento, A_domicilio, A_lu, A_estado) values ('44213659','Luciano','armani','pepig@gmail.com','3883645879','04-06-02','calle Salta','4987',b'1');
+insert into alumnos (A_dni, A_nombre, A_apellido, A_email, A_telefono, A_fecha_nacimiento, A_domicilio, A_lu, A_estado) values ('43654987','Ezequiel','funcio','TomasPala@gmail.com','3889865321','98-04-06','calle facio','4658',b'1');
+
 insert into docente (D_legajo, D_nombre, D_apellido, D_email, D_telefono, D_estado) values ('1101', 'Lucas', 'cerros', 'Lucasdelcerro@gmail.com','3882635487',b'1');
 insert into docente (D_legajo, D_nombre, D_apellido, D_email, D_telefono, D_estado) values ('1102', 'fabrizio', 'armada', 'fabrizioarmada3@gmail.com','3882635487',b'1');
 insert into docente (D_legajo, D_nombre, D_apellido, D_email, D_telefono, D_estado) values ('1103', 'Bugs', 'Bunny', 'BunnyBugs@gmail.com','3882635487',b'1');
+insert into docente (D_legajo, D_nombre, D_apellido, D_email, D_telefono, D_estado) values ('1104', 'Marcelo', 'Ines', 'IMarcelo@gmail.com','3887845112',b'1');
+insert into docente (D_legajo, D_nombre, D_apellido, D_email, D_telefono, D_estado) values ('1105', 'Joaquin', 'Sanchez', 'SancheJoaquin@gmail.com','3882635487',b'1');
+insert into docente (D_legajo, D_nombre, D_apellido, D_email, D_telefono, D_estado) values ('1106', 'Ramon', 'Jeremiaz', 'JRamon@gmail.com','3886932547',b'1');
+insert into docente (D_legajo, D_nombre, D_apellido, D_email, D_telefono, D_estado) values ('1107', 'Ezequiel', 'Palacion', 'PEzequiel@gmail.com','3885463257',b'1');
+insert into docente (D_legajo, D_nombre, D_apellido, D_email, D_telefono, D_estado) values ('1108', 'Bruno', 'Lopez', 'LBruno@gmail.com','3882032154',b'1');
+insert into docente (D_legajo, D_nombre, D_apellido, D_email, D_telefono, D_estado) values ('1109', 'Lucas', 'Gomez', 'GLucas@gmail.com','3889785645',b'1');
+insert into docente (D_legajo, D_nombre, D_apellido, D_email, D_telefono, D_estado) values ('1110', 'Luis', 'Martinez', 'mLuis@gmail.com','3886452125',b'1');
+
 insert into materias (M_codigo,M_nombre,M_curso,M_cantidad_horas,M_modalidad,M_estado,D_id,C_id) values ('1111','PE','1er',6,'virtual',b'1',1,1);
 insert into materias (M_codigo,M_nombre,M_curso,M_cantidad_horas,M_modalidad,M_estado,D_id,C_id) values ('1112','Qumica I','1er',7,'Presencial',b'1',2,3);
 insert into materias (M_codigo,M_nombre,M_curso,M_cantidad_horas,M_modalidad,M_estado,D_id,C_id) values ('1113','IA','3er',6,'virtual',b'1',3,2);
+insert into materias (M_codigo,M_nombre,M_curso,M_cantidad_horas,M_modalidad,M_estado,D_id,C_id) values ('1114','Analisis Matematico','1er',6,'Presencial',b'1',4,5);
+insert into materias (M_codigo,M_nombre,M_curso,M_cantidad_horas,M_modalidad,M_estado,D_id,C_id) values ('1115','Qumica II','1er',7,'Presencial',b'1',5,3);
+insert into materias (M_codigo,M_nombre,M_curso,M_cantidad_horas,M_modalidad,M_estado,D_id,C_id) values ('1116','Fisica I','1er',6,'Presencial',b'1',6,2)
+insert into materias (M_codigo,M_nombre,M_curso,M_cantidad_horas,M_modalidad,M_estado,D_id,C_id) values ('1117','Dibujo Tecnico','1er',6,'virtual',b'1',7,10);
+insert into materias (M_codigo,M_nombre,M_curso,M_cantidad_horas,M_modalidad,M_estado,D_id,C_id) values ('1118','Circuitos I','1er',7,'Presencial',b'1',8,10);
+insert into materias (M_codigo,M_nombre,M_curso,M_cantidad_horas,M_modalidad,M_estado,D_id,C_id) values ('1119','POO','2er',6,'Presencial',b'1',9,9);
+insert into materias (M_codigo,M_nombre,M_curso,M_cantidad_horas,M_modalidad,M_estado,D_id,C_id) values ('1110','Geometria','2do',6,'virtual',b'1',10,5);
